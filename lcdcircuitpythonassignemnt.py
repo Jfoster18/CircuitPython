453fa647beaf6bfafceb43cd5b582e76daa7ccc8
Jackson D Foster <jfoster18@ccs.local>
2019-10-07 09:43:01 -0400

    my lcd code for the circuitpython assignemtns

---
+++ lcdcircuitpythonassignemnt.py
@@ -0,0 +1,40 @@
+import board
+import time
+import digitalio
+from lcd.lcd import LCD
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+from lcd.lcd import CursorMode
+from analogio import AnalogOut
+
+analog_out=AnalogOut(board.A0)
+
+lcd = LCD(I2CPCF8574Interface(0x27), num_rows=2, num_cols=16)
+
+button_a = digitalio.DigitalInOut(board.D1)
+button_a.direction = digitalio.Direction.INPUT
+button_a.pull = digitalio.Pull.DOWN
+
+button_b = digitalio.DigitalInOut(board.D2)
+button_b.direction = digitalio.Direction.INPUT
+button_b.pull = digitalio.Pull.DOWN
+
+up = 1
+number = 0
+
+while True:
+
+    lcd.set_cursor_pos(0,0)
+    lcd.print("Switch State: ")
+    lcd.print(str(up))
+    lcd.set_cursor_pos(1,0)
+    lcd.print("presses:")
+
+    if button_a.value:
+        number += up
+
+
+
+    if button_b.value:
+        up *= -1
+    lcd.print(str(number))
+    lcd.print("     ")
